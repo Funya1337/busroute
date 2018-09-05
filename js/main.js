0d138b0cc510a9863cc8b1f46c5a072a7db39251
Funya1337 <funya7654@gmail.com>
2018-09-05 21:29:18 +0300

    visual part + backend

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+let startId, endId;
+const arrNames = ["Ryazansky Prospect", "Kuzminki", "Tekstilshchiki", "Volgogradsky Prospekt", "Proletarskaya", "Barrikadnaya"];
+const arr = ["R", "K", "T", "V", "P", "B"];
+class Route {
+	constructor(arrNames, arr, timeInterval) {
+		this.arrNames = arrNames;
+		this.arr = arr;
+		this.timeInterval = timeInterval;
+	}
+	CheckInterval(startPoint, endPoint) {
+		let indexStart = this.arr.indexOf(startPoint);
+		let indexEnd = this.arr.indexOf(endPoint);
+		let resTime = Math.abs((indexStart - indexEnd) * this.timeInterval);
+		console.log(`nearest bus will come through ${resTime} seconds`);
+	}
+}
+let stations = new Route(arrNames, arr, 20);
+
+function getAllStations() {
+	document.getElementById('stations').innerHTML = arrNames;
+}
+function take(id){
+	startId = id;
+	console.log(startId)
+};
+
+function take2(id2){
+	endId = id2;
+	console.log(endId);
+};
+function main() {
+	let startPointCap = startId;
+	let endPointCap = endId;
+	stations.CheckInterval(startPointCap, endPointCap);
+}
